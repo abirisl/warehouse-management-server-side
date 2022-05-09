1df196d287074f9a2a1e4cf77877d938c783d639
abir islam <mhabir979292@gmail.com>
2022-05-09 14:34:42 +0600

    db connected

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const { MongoClient, ServerApiVersion } = require('mongodb');
 require('dotenv').config();
 const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.jgepi.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  console.log('db connected')
+  // perform actions on the collection object
+  client.close();
+});
 
 app.get('/', (req,res) =>{
     res.send('running bike warehouse')
@@ -14,3 +25,6 @@ app.get('/', (req,res) =>{
 app.listen(port, () =>{
     console.log('this is port', port)
 })
+
+
+
